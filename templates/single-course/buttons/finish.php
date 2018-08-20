529636a17dc704e3581cdb1d0165a1e1c032b688
Nick Williams <nick.williams@striderhq.local>
2018-08-20 10:43:03 -0600

    commented out finish course html at template level

--- templates/single-course/buttons/finish.php
+++ templates/single-course/buttons/finish.php
@@ -17,14 +17,14 @@ defined( 'ABSPATH' ) || exit();
 $course = LP_Global::course();
 $user   = LP_Global::user();
 ?>
-
+<!--
 <form class="lp-form form-button form-button-finish-course" method="post"
-      data-confirm="<?php LP_Strings::esc_attr_e( 'confirm-finish-course', '', array( $course->get_title() ) ); ?>">
+      data-confirm="<?php //LP_Strings::esc_attr_e( 'confirm-finish-course', '', array( $course->get_title() ) ); ?>">
 
-    <button class="lp-button"><?php _e( 'Finish course', 'learnpress' ); ?></button>
-    <input type="hidden" name="course-id" value="<?php echo $course->get_id(); ?>"/>
+    <button class="lp-button"><?php //_e( 'Finish course', 'learnpress' ); ?></button>
+    <input type="hidden" name="course-id" value="<?php //echo $course->get_id(); ?>"/>
     <input type="hidden" name="finish-course-nonce"
-           value="<?php echo esc_attr( wp_create_nonce( sprintf( 'finish-course-%d-%d', $course->get_id(), $user->get_id() ) ) ); ?>"/>
+           value="<?php //echo esc_attr( wp_create_nonce( sprintf( 'finish-course-%d-%d', $course->get_id(), $user->get_id() ) ) ); ?>"/>
     <input type="hidden" name="lp-ajax" value="finish-course"/>
     <input type="hidden" name="noajax" value="yes"/>
 
